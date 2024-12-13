6be0f377e12661ccab4da8ba031e8cf54775419b
sultan1k <artiomkratov@gmail.com>
2024-12-13 16:08:55 +0300

    [RFN-3] Create header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,22 @@
-'use client';
-import Contacts from './pages/Contacts';
+import { TitleBlock } from "../components/widgets";
+import Fullscreen from "../components/shared/Fullscreen";
+import { InfoBlock } from "../components/features";
 
 export default function Home() {
-  return <Contacts/>;
-}
+  return (
+    <>
+    <Fullscreen background="/backgrounds/1st.png" needsTopShadow needsBottomShadow>
+      <TitleBlock />
+    </Fullscreen>
+    <Fullscreen background="/backgrounds/2nd.png" needsTopShadow>
+      <InfoBlock
+        title="НОВОСТИ"
+        children="News"
+      />
+    </Fullscreen>
+    <Fullscreen background="/backgrounds/3rd.png" />
+    <Fullscreen background="/backgrounds/4th.png" />
+    </>
+  );
+};
+
---
+++ src/components/shared/Typography/index.tsx
@@ -0,0 +1,7 @@
+import Text from "./Text";
+import Bold from "./Bold";
+
+export {
+  Text,
+  Bold
+};
