c7e858026a1649f892d691ec8e226dacf5c3fef8
Robocotik <alexander.starkin@gmail.com>
2024-12-19 22:11:38 +0300

    fix(conponents): fix contact height

--- src/app/pages/Contacts/components/Card/index.tsx
+++ src/app/pages/Contacts/components/Card/index.tsx
@@ -4,7 +4,7 @@ import Props from './Card.props';
 const Card: FC<Props> = ({children, className, ...props}) => {
   return (
     <div
-      className={`border-orange_main h-full border-5 rounded-3xl z-10 ${className}`}
+      className={`border-orange_main  border-5 rounded-3xl z-10 ${className}`}
       {...props}>
       {children}
     </div>
