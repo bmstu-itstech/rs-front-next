46872d4e83d9ef8b1957cf02c5fd80bd9adea9c4
Robocotik <alexander.starkin@gmail.com>
2024-12-19 22:10:12 +0300

    feat(components): add Contacts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
-import Image from 'next/image';
-import News from './pages/News';
-import { usecase_default } from './components/Carousel/Carousel.usecase';
+import Contacts from './pages/Contacts';
+
 export default function Home() {
-  return <News {...usecase_default} />;
+  return <Contacts/>;
 }
