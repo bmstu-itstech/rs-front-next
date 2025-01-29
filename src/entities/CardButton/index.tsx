53a5d0174c637efb93f89e628dd96bc848deb6f9
sultan1k <artiomkratov@gmail.com>
2025-01-29 20:29:54 +0300

    added Dockerfile

---
+++ src/entities/CardButton/index.tsx
@@ -0,0 +1,21 @@
+import { memo, MouseEventHandler } from "preact/compat";
+
+interface CardButtonProps {
+  label: string;
+  onClick: MouseEventHandler<HTMLDivElement>;
+};
+
+const CardButton: React.FC<CardButtonProps> = ({
+  label,
+  onClick
+}) => {
+
+  return (
+    <div class="card-button" onClick={onClick}>
+      {label}
+    </div>
+  );
+
+};
+
+export default memo(CardButton);
